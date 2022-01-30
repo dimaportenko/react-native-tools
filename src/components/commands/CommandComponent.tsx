365278f8709f71ff0e7f851be4863e932ad67941
Dima Portenko <troublesohard@gmail.com>
2022-01-30 16:38:07 +0200

    feat: collapse terminal output

--- src/components/commands/CommandComponent.tsx
+++ src/components/commands/CommandComponent.tsx
@@ -1,7 +1,7 @@
 /**
  * Created by Dima Portenko on 22.01.2022
  */
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, View} from 'react-native';
 import {observer} from 'mobx-react-lite';
 import {useStore} from '../../store/RootStore';
@@ -13,6 +13,7 @@ import {Spacer} from '../markup/Spacer';
 interface CommandComponentProps {}
 
 export const CommandComponent = observer((props: CommandComponentProps) => {
+  const [expand, setExpand] = useState(false);
   const {project} = useStore();
   const {current} = project;
 
@@ -27,26 +28,49 @@ export const CommandComponent = observer((props: CommandComponentProps) => {
       border rounded-5px border-gray-400 dark:border-gray-600 
       bg-gray-300 dark:bg-gray-900 dark:bg-opacity-40
       `}>
-      <View style={tw`flex-row pt-2px pb-5px items-center`}>
+      <View style={tw`flex-row pt-2px pb-2px items-center`}>
+
         <Text style={tw.style({fontSize: 18})}>Bundler</Text>
         <Spacer size={8} />
         <HoverHighlighButton
           onPress={() => {
             project.startBundler(current);
           }}
-          tintColor="#499C54"
+          // tintColor="#499C54"
           imageSource={require('../../../assets/icons/ic_play_arrow_48px.png')}
         />
         <HoverHighlighButton
           disabled={!current.bundlerCommand.isRunning}
           onPress={current.bundlerCommand.stop}
-          tintColor="#C75450"
+          // tintColor="#C75450"
           imageSource={require('../../../assets/icons/ic_stop_48px.png')}
         />
+        <View style={tw`flex-1`} />
+        <View
+          style={tw.style(
+            `w-14px h-14px rounded-8px`,
+            project.current?.bundlerCommand.isRunning
+              ? 'bg-green-500'
+              : 'bg-red-500',
+          )}
+        />
+        <Spacer size={8} />
+        <HoverHighlighButton
+          // disabled={!current.bundlerCommand.isRunning}
+          onPress={() => setExpand(!expand)}
+          // tintColor="#C75450"
+          imageSource={
+            expand
+              ? require('../../../assets/icons/ic_expand_more_48px.png')
+              : require('../../../assets/icons/ic-expand-less-48px.png')
+          }
+        />
       </View>
-      <ScrollView style={tw`w-100% h-300px bg-white dark:bg-black`}>
-        <Text>{current.bundlerCommand.output}</Text>
-      </ScrollView>
+      {expand && (
+        <ScrollView style={tw`w-100% h-300px bg-white dark:bg-black mt-3px`}>
+          <Text>{current.bundlerCommand.output}</Text>
+        </ScrollView>
+      )}
     </View>
   );
 });
